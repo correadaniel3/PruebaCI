3d9651550639e1e1e6688f321c25b96f5c7385a2
correadaniel3 <correadaniel3@gmail.com>
2018-01-05 15:07:25 -0500

    initial commit

---
+++ src/main/java/co/ceiba/PruebaCI/PruebaCiApplication.java
@@ -0,0 +1,12 @@
+package co.ceiba.PruebaCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCiApplication.class, args);
+	}
+}
